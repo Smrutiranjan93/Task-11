f8200e4453d004dff709b370bf217f0e75c38fc1
Smrutiranjan93 <103051369+Smrutiranjan93@users.noreply.github.com>
2022-08-05 20:05:53 +0530

    first commit

--- storage.js
+++ storage.js
@@ -7,11 +7,28 @@ function tolocalStorage(event){
 event.preventDefault();
 const name=event.target.name.value;
 const email=event.target.email.value;
-localStorage.setItem('name',name);
-localStorage.setItem('email',email);
+// localStorage.setItem('name',name);
+// localStorage.setItem('email',email);
 const obj={
     name:name,
     email:email
 };
-localStorage.setItem('userDetails',JSON.stringify(obj));
+localStorage.setItem(obj.email,JSON.stringify(obj));
+getInfoOnScreen(obj);
 }
+
+//as every user don't have a particular id,all old user get replaced by new user.
+
+//storing multiple user
+// localStorage.setItem(obj.email,JSON.stringify(obj));
+
+
+function getInfoOnScreen(user){
+   const parentNode =document.getElementById('listOfUsers');
+   const childHTML=`<li> ${user.name} -${user.email} </li>`;
+   parentNode.innerHTML=parentNode.innerHTML+childHTML;
+
+}
+
+
+ 
